947be96c1ed9cd8a721a5b844f6d85624a1fc456
Stefan-Nikola <s.stanev@student.fontys.nl>
2024-05-29 00:29:06 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    for(int i = 0; i < 1000000; i++) {
+        std::cout << i << std::endl;
+    }
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}

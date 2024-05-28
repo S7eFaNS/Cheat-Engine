6291c8fe62cb93b6b4894ec9d6c377a47f64db0a
S7eFaNS <stefannikola298@gmail.com>
2024-05-29 00:49:38 +0200

    something

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
 int main() {
-    for(int i = 0; i < 1000000; i++) {
-        std::cout << i << std::endl;
-    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
